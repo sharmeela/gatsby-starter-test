d67953dc588643a9ae5dc411a02d731415d899d6
Sharmeela <sharmeela.jaimalani@gmail.com>
2021-08-17 19:51:16 +0530

    remote node grapgQL

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,7 @@ const { createRemoteFileNode } = require(`gatsby-source-filesystem`);
 
 module.exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions
+	//console.log('node.internal.type', node.internal.type);
     if (node.internal.type === 'MarkdownRemark') {
         const slug = path.basename(node.fileAbsolutePath, '.md')
         
@@ -14,49 +15,38 @@ module.exports.onCreateNode = ({ node, actions }) => {
         })
     }
 },
-
+/*
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
     const blogTemplate = path.resolve('./src/templates/blog.js');
-    const productTemplate = path.resolve('./src/templates/product.js');
-    const imgTemplate = path.resolve('./src/templates/imageProduct.js');
+   // const productTemplate = path.resolve('./src/templates/product.js');
+    //const imgTemplate = path.resolve('./src/templates/imageProduct.js');
     await graphql(`
             {
-                allMarkdownRemark {
-                    edges {
-                        node {
-                            fields {
-                                slug
-                            }
+                /*allMarkdownRemark{
+                edges{
+                    node{
+                        frontmatter{
+                            title,
+                            date
                         }
-                    }
-                }
-                allProducts {
+						fields {
+							slug
+							}
+						}
+					}
+				}
+				allProductsByApi {
                     edges {
-                        node {
-                            details
+                        node {                            
                             id
-                            id__normalized
-                            price
-                            slug
-                            sku
                             title
                         }
                     }
                 }
             }
         `).then(res => {
-            res.data.allProducts.edges.forEach(async edge => {
-                createPage({
-                    component: productTemplate,
-                    path:`/products/${edge.node.id__normalized}`,
-                    context: {
-                        id__normalized: edge.node.id__normalized
-                    }
-              })
-            })
-
-            res.data.allMarkdownRemark.edges.forEach((edge) => {
+			/*res.data.allMarkdownRemark.edges.forEach((edge) => {
                 createPage({
                     component: blogTemplate,
                     path: `/blog/${edge.node.fields.slug}`,
@@ -64,6 +54,142 @@ module.exports.createPages = async ({ graphql, actions }) => {
                         slug: edge.node.fields.slug
                     }
                 })
+            })*/            
+
+            /*
+			res.data.allProductsByApi.edges.forEach(async edge => {
+                createPage({
+                    component: productTemplate,
+                    path:`/products/${edge.node.id}`,
+                    context: {
+                        id: edge.node.id
+                    }
+              })
             })
+			
         })
+}*/
+
+// please comment this section to check wp-graphql queries
+module.exports.createResolvers = ({
+  getNodesByType,
+  actions,
+  cache,
+  createNodeId,
+  createResolvers,
+  store,
+  reporter,
+}) => {
+  const { createNode, deleteNode  } = actions
+  createResolvers({
+    mediaAll: {
+      imageFile: {
+        type: `File`,
+        async resolve(source, args, context, info) {
+			//const productsApiNode = getNodesByType("ProductsApi")
+			console.log('source', source);
+			//if(productsApiNode){
+				try {
+					
+					let getUrlIndex = source.src.search("cloudfront");
+					console.log('getUrlIndex', getUrlIndex);
+					if(getUrlIndex > 0){
+					  baseUrlPath = source.src;
+					}else if(source.src.search("images.pexels.com") > 0){
+						baseUrlPath = source.src;
+					}else{
+					  baseUrlPath = "https://stellalunaback.debugme.in/wp-content/uploads/" + source.src;
+					}
+				  const remoteFileNode = await createRemoteFileNode({
+					url: `${baseUrlPath}`,
+					store,
+					cache,
+					createNode,
+					createNodeId,
+					reporter,
+				  }, () => console.log(this))
+				  return remoteFileNode;			  
+				}catch (err) {
+					console.log(err);
+				}
+			//}
+			return null;
+			
+        },
+      },
+    },
+	mediaWide: {
+      imageFile: {
+        type: `File`,
+        async resolve(source, args, context, info) {
+			try {
+				//console.log('source', source);
+				let getUrlIndex = source.src.search("cloudfront");
+				if(getUrlIndex > 0){
+				  baseUrlPath = source.src;
+				}else
+				{
+				  baseUrlPath = "https://stellalunaback.debugme.in/wp-content/uploads/" + source.src;
+				}
+			  const remoteFileNode = await createRemoteFileNode({
+				url: `${baseUrlPath}`,
+				store,
+				cache,
+				createNode,
+				createNodeId,
+				reporter,
+			  })
+			  return remoteFileNode;			  
+			}catch (err) {
+                console.log('err', err);
+			}
+			return null;
+			
+        },
+      },
+    },
+  })
 }
+
+ //Uncomment this to check the wp-graphql node
+ /*
+exports.createResolvers = async (
+  {
+    actions,
+    cache,
+    createNodeId,
+    createResolvers,
+    store,
+    reporter,
+  },
+) => {
+  const { createNode } = actions
+
+  await createResolvers({
+    WPGraphQL_Product.galleryImages: {
+      imageFile: {
+        type: "File",
+        async resolve(source) {
+			console.log('source', source);
+          let sourceUrl = source.sourceUrl
+
+          if (source.mediaItemUrl !== undefined) {
+            sourceUrl = source.mediaItemUrl
+          }
+
+          return await createRemoteFileNode({
+            url: encodeURI(sourceUrl),
+            store,
+            cache,
+            createNode,
+            createNodeId,
+            reporter,
+          })
+        },
+      },
+    },
+  })
+}
+*/
+
+
